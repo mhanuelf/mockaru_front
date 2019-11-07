49aaf8fc62d06be6db7a2329fd45ec5d064419bf
Manuel Montanez <mhanuel@gmail.com>
2019-11-07 14:22:47 -0500

    Se implementa configuracion de la url del backend

--- src/app/pqr/pqr.service.ts
+++ src/app/pqr/pqr.service.ts
@@ -3,13 +3,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PqrService {
 
-  uri = 'http://127.0.0.1:3000/dashboard/pqr/producttype';
+  uri =  environment.apiUrl+'/pqr/producttype';
 
 
   constructor(private http: HttpClient, private router: Router) { }
